3ec31ba6761f0d4a07e1580762afa310252f9581
Manuel V. Montesinos <38406081+manuelmontesinos@users.noreply.github.com>
2021-07-19 16:40:10 +0200

    Updated README and code description.

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # Quantitative Economics
 A collection of computational methods for conducting research in economics and finance. Files included:
 
+## Discrete Choice Models
+- Estimation of a multinomial logit model of insurance choice: [mlogit_insurance.do](https://github.com/manuelmontesinos/quant_econ/blob/main/discrete_choice/multinomial_logit/mlogit_insurance.do) (Stata), [mlogit_insurance.m](https://github.com/manuelmontesinos/quant_econ/blob/main/discrete_choice/multinomial_logit/mlogit_insurance.m) (Matlab).
+-- For optimization based on the BFGS algorithm, use this Matlab function to compute the log-likelihood: [mlogit_insurance_llike.m](https://github.com/manuelmontesinos/quant_econ/blob/main/discrete_choice/multinomial_logit/mlogit_insurance_llike.m).
+-- For optimization based on the Newton-Raphson algorithm, use this Matlab function: [mlogit_nr.m](https://github.com/manuelmontesinos/quant_econ/blob/main/discrete_choice/multinomial_logit/mlogit_nr.m).
+
 ## The Static General Equilibrium Model
 - Social planner solution to the static general equilibrium model: [planner_static_1.R](https://github.com/montesinosmv/quant_econ/blob/master/static_ge_model/planner_static_1.R) (R), [planner_static_1.py](https://github.com/manuelmontesinos/quant_econ/blob/main/static_ge_model/planner_static_1.py) (Python).
 - Market solution to the static general equilibrium model: [market_static_1.R](https://github.com/montesinosmv/quant_econ/blob/master/static_ge_model/market_static_1.R) (R), [market_static_1.py](https://github.com/manuelmontesinos/quant_econ/blob/main/static_ge_model/market_static_1.py) (Python).
--- discrete_choice/multinomial_logit/mlogit_nr.m
+++ discrete_choice/multinomial_logit/mlogit_nr.m
@@ -1,14 +1,14 @@
 function [best, sebest, llik] = mlogit_nr(yobs, xobs, b0)
 %==========================================================================
 % FUNCTION: Estimate the a multinomial logit model by maximum likelihood,
-% minimizing the function by Newton-Raphson
-% 
+% using the Newton-Raphson algorithm for optimization 
+%
 % AUTHOR: Manuel V. Montesinos (Universitat Autonoma de Barcelona and
 % Barcelona GSE)
 %
 % THIS VERSION: July 2021
 %
-% INPUT: 
+% INPUT:
 %   yobs <- (nosb-by-1) vector of observations of the dependent variable,
 %           i.e., indicator of discrete choice (1, 2, ..., nalt)
 %   xobs <- (nobs-by-k) matrix of explanatory variables
@@ -17,7 +17,7 @@ function [best, sebest, llik] = mlogit_nr(yobs, xobs, b0)
 %
 % OUTPUT:
 %   best   <- (k*(nalt-1)-by-1) vector of parameter estimates
-%   sebest <- Standard errors 
+%   sebest <- Standard errors
 %   llik   <- Value of the log-likelihood in the maximum
 %==========================================================================
 
@@ -34,13 +34,13 @@ criter = 1000;
 
 % Start optimization
 while criter > eps
-    
+
     % Compute conditional choice probabilities
     b0 = [zeros(kpar, 1) reshape(b0, kpar, nalt-1)];
     xb = xobs*b0;
     sumexpe = sum(exp(xb'), 1);
     pxb = exp(xb)./transpose(sumexpe);
-    
+
     % Compute the log-likelihood
     myzero = 1e-16;
     llik = 0;
@@ -50,7 +50,7 @@ while criter > eps
       llik = llik+sum((yobs==jj).*log(pxb(:,jj)+myzero), 1);
       jj = jj+1;
     end
-    
+
     % Compute the gradient and the hessian
     d1like = zeros(kpar*(nalt-1), 1);
     d2like = zeros(kpar*(nalt-1), kpar*(nalt-1));
@@ -75,7 +75,7 @@ while criter > eps
       jj = jj+1;
     end
 
-    % Update criterion, parameter values and iteration 
+    % Update criterion, parameter values and iteration
     b0 = reshape(b0(:, 2:end), kpar*(nalt-1), 1);
     b1 = b0 - inv(d2like)*d1like;
     criter = sqrt((b1-b0)'*(b1-b0));
